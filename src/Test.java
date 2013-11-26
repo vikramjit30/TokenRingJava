a864e1180929731422a4ab25a733056a7977d0d4
moerov <moyorov@gmail.com>
2013-11-26 22:36:35 +0100

    First commit. Start coding.

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrey
+ * Date: 11/26/13
+ * Time: 10:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("It works.");
+    }
+}
